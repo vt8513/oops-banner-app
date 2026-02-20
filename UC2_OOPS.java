1d05d0f385d38b1129b47f412f2db9dd3d646117
vt8513 <vt8513@srmist.edu.in>
2026-02-20 16:11:40 +0530

    Add files via upload

---
+++ UC2_OOPS.java
@@ -0,0 +1,14 @@
+public class UC2_OOPS {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****     *****     ***** ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println("*     *   *     *   *****      ***** ");
+        System.out.println("*     *   *     *   *               *");
+        System.out.println("*     *   *     *   *               *");
+        System.out.println(" *****     *****    *         *****  ");
+
+    }
+}
