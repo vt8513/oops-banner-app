0c1349b6ebf443971a47789b285ca6ea4e18af6e
vt8513 <vt8513@srmist.edu.in>
2026-02-20 14:17:28 +0530

    Add files via upload

---
+++ OOPSbannerApp uc1.java
@@ -0,0 +1,21 @@
+public class OOPSbannerApp {
+    /**
+ * OOPSBannerApp
+ *
+ * Use Case 1 (UC1):
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Vrinda
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+}
