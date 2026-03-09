5c136ea4e7dfb3418fd4716eefada0bb75b53cc2
vt8513 <vt8513@srmist.edu.in>
2026-03-09 19:28:07 +0530

    Add files via upload

---
+++ uc2.java
@@ -0,0 +1,14 @@
+public class uc2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    *****  ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *        *     *");
+        System.out.println(" *****    *****   *         ***** ");
+
+    }
+}
